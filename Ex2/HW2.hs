cfecfa802bce6c0ff6d17e63817cf8a063e65ecc
Omri-Lvy <omri.lvy@gmail.com>
2024-05-04 11:26:37 +0300

    Added partial section 1.1

--- Ex2/HW2.hs
+++ Ex2/HW2.hs
@@ -14,53 +14,67 @@ import Prelude (Bool (..), Bounded (..), Char, Either (..), Enum (..), Eq (..), 
 
 -- Section 1.1: Basic Maybes
 concatMaybeMap :: (a -> Maybe b) -> Maybe a -> Maybe b
+concatMaybeMap _ Nothing = Nothing
+concatMaybeMap f (Just x) = f x
+
 fromMaybe :: a -> Maybe a -> a
+fromMaybe defaultValue Nothing = defaultValue
+fromMaybe _ (Just x) = x
+
 maybe :: b -> (a -> b) -> Maybe a -> b
-catMaybes :: [Maybe a] -> [a]
+maybe defaultValue _ Nothing = defaultValue
+maybe _ f (Just x) = f x
+
+-- catMaybes :: [Maybe a] -> [a]
 mapMaybe :: (a -> Maybe b) -> [a] -> [b]
--- Section 1.2 Basic Eithers
-concatEitherMap :: (a -> Either e b) -> Either e a -> Either e b
-either :: (a -> c) -> (b -> c) -> Either a b -> c
-mapLeft :: (a -> c) -> Either a b -> Either c b
-catEithers :: [Either e a] -> Either e [a]
-mapEither :: (a -> Either e b) -> [a] -> Either e [b]
-partitionEithers :: [Either a b] -> ([a], [b])
-eitherToMaybe :: Either a b -> Maybe b
+mapMaybe _ [] = []
+mapMaybe f (x : xs) = case f x of
+  Nothing -> mapMaybe f xs
+  Just y -> y : mapMaybe f xs
 
--- Section 2: Lists
-take :: Int -> [a] -> [a]
-takeWhile :: (a -> Bool) -> [a] -> [a]
-drop :: Int -> [a] -> [a]
-dropWhile :: (a -> Bool) -> [a] -> [a]
-reverse :: [a] -> [a]
-rotate :: Int -> [a] -> [a]
-lotate :: Int -> [a] -> [a]
-type Generator a = (a -> a, a -> Bool, a)
-fromGenerator :: Generator a -> [a]
-replicate :: Int -> a -> [a]
-inits :: [a] -> [[a]]
-tails :: [a] -> [[a]]
+-- -- Section 1.2 Basic Eithers
+-- concatEitherMap :: (a -> Either e b) -> Either e a -> Either e b
+-- either :: (a -> c) -> (b -> c) -> Either a b -> c
+-- mapLeft :: (a -> c) -> Either a b -> Either c b
+-- catEithers :: [Either e a] -> Either e [a]
+-- mapEither :: (a -> Either e b) -> [a] -> Either e [b]
+-- partitionEithers :: [Either a b] -> ([a], [b])
+-- eitherToMaybe :: Either a b -> Maybe b
 
--- Section 3: zips and products
-zipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
-zip :: [a] -> [b] -> [(a, b)]
-zipFill :: a -> b -> [a] -> [b] -> [(a, b)]
-data ZipFail = ErrorFirst | ErrorSecond deriving (Eq, Show)
-zipFail :: [a] -> [b] -> Either ZipFail [(a, b)]
-unzip :: [(a, b)] -> ([a], [b])
+-- -- Section 2: Lists
+-- take :: Int -> [a] -> [a]
+-- takeWhile :: (a -> Bool) -> [a] -> [a]
+-- drop :: Int -> [a] -> [a]
+-- dropWhile :: (a -> Bool) -> [a] -> [a]
+-- reverse :: [a] -> [a]
+-- rotate :: Int -> [a] -> [a]
+-- lotate :: Int -> [a] -> [a]
+-- type Generator a = (a -> a, a -> Bool, a)
+-- fromGenerator :: Generator a -> [a]
+-- replicate :: Int -> a -> [a]
+-- inits :: [a] -> [[a]]
+-- tails :: [a] -> [[a]]
 
--- Section 4: Knight travels
--- Position (0, 0) is the top-left corner.
-data KnightPos = KnightPos {x :: Int, y :: Int} deriving (Show, Eq)
-data KnightMove = TopLeft | TopRight | RightTop | RightBottom | BottomRight | BottomLeft | LeftBottom | LeftTop deriving (Enum, Bounded, Show, Eq)
--- Utility to get all knight moves. Don't worry about the implementation of this.
-allKnightMoves :: [KnightMove]
-allKnightMoves = [minBound .. maxBound]
-data Board = Board {width :: Int, height :: Int} deriving (Show, Eq)
-tour :: Board -> KnightPos -> Maybe [KnightMove]
-newtype InvalidPosition = InvalidPosition KnightPos deriving (Show, Eq)
-translate :: KnightPos -> [KnightMove] -> [KnightPos]
-translate' :: [KnightPos] -> Either InvalidPosition [KnightMove]
+-- -- Section 3: zips and products
+-- zipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+-- zip :: [a] -> [b] -> [(a, b)]
+-- zipFill :: a -> b -> [a] -> [b] -> [(a, b)]
+-- data ZipFail = ErrorFirst | ErrorSecond deriving (Eq, Show)
+-- zipFail :: [a] -> [b] -> Either ZipFail [(a, b)]
+-- unzip :: [(a, b)] -> ([a], [b])
 
--- Bonus (10 points)
-mark :: Board -> [KnightPos] -> Either InvalidPosition [[Int]]
+-- -- Section 4: Knight travels
+-- -- Position (0, 0) is the top-left corner.
+-- data KnightPos = KnightPos {x :: Int, y :: Int} deriving (Show, Eq)
+-- data KnightMove = TopLeft | TopRight | RightTop | RightBottom | BottomRight | BottomLeft | LeftBottom | LeftTop deriving (Enum, Bounded, Show, Eq)
+-- -- Utility to get all knight moves. Don't worry about the implementation of this.
+-- allKnightMoves :: [KnightMove]
+-- allKnightMoves = [minBound .. maxBound]
+-- data Board = Board {width :: Int, height :: Int} deriving (Show, Eq)
+-- tour :: Board -> KnightPos -> Maybe [KnightMove]
+-- newtype InvalidPosition = InvalidPosition KnightPos deriving (Show, Eq)
+-- translate :: KnightPos -> [KnightMove] -> [KnightPos]
+-- translate' :: [KnightPos] -> Either InvalidPosition [KnightMove]
+
+-- -- Bonus (10 points)
+-- mark :: Board -> [KnightPos] -> Either InvalidPosition [[Int]]
