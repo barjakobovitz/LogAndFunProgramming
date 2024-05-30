9b59a2d0af9bc412d42d70006f39d0932ef31f60
Omri-Lvy <omri.lvy@gmail.com>
2024-05-30 17:52:48 +0300

    HW4 implement EqSet and EqMap

--- HW4/EqMap.hs
+++ HW4/EqMap.hs
@@ -2,16 +2,17 @@
 {-# OPTIONS_GHC -Wall -Werror #-}
 {-# OPTIONS_GHC -Wno-unused-imports #-}
 
-module EqMap (
-  EqMap,
-  CombiningMap (..),
-  empty,
-  EqMap.insert, -- To avoid name clash with Data.List.insert
-  member,
-  remove,
-  EqMap.lookup, -- To avoid name clash with Prelude.lookup
-  assocs
-) where
+module EqMap
+  ( EqMap,
+    CombiningMap (..),
+    empty,
+    EqMap.insert, -- To avoid name clash with Data.List.insert
+    member,
+    remove,
+    EqMap.lookup, -- To avoid name clash with Prelude.lookup
+    assocs,
+  )
+where
 
 import Data.Either
 import Data.List
@@ -20,19 +21,57 @@ import Data.Semigroup (Arg (..))
 import EqSet (EqSet)
 import EqSet qualified
 
-data EqMap k v -- Complete me!
+data EqMap k v = EqMap (EqSet (Arg k v))
 
 empty :: EqMap k v
-member :: Eq k => k -> EqMap k v -> Bool
-insert :: Eq k => k -> v -> EqMap k v -> EqMap k v
-remove :: Eq k => k -> EqMap k v -> EqMap k v
-lookup :: Eq k => k -> EqMap k v -> Maybe v
-assocs :: EqMap k v -> [(k, v)]
+empty = EqMap EqSet.empty
 
-instance (Eq k, Eq v) => Eq (EqMap k v)
-instance (Show k, Show v) => Show (EqMap k v)
-instance Eq k => Semigroup (EqMap k v)
-instance Eq k => Monoid (EqMap k v)
+member :: (Eq k) => k -> EqMap k v -> Bool
+member k (EqMap s) = any (\(Arg k' _) -> k == k') (EqSet.elems s)
+
+insert :: (Eq k) => k -> v -> EqMap k v -> EqMap k v
+insert key value (EqMap s) =
+  let s' = EqSet.remove (Arg key undefined) s -- Remove any existing entry with the same key
+   in EqMap (EqSet.insert (Arg key value) s') -- Insert the new key-value pair
+
+remove :: (Eq k) => k -> EqMap k v -> EqMap k v
+remove k (EqMap s) = EqMap (EqSet.remove (Arg k undefined) s)
+
+lookup :: (Eq k) => k -> EqMap k v -> Maybe v
+lookup key (EqMap set) = fmap (\(Arg _ v) -> v) . find (\(Arg k _) -> k == key) $ EqSet.elems set
+
+assocs :: EqMap k v -> [(k, v)]
+assocs (EqMap s) = map (\(Arg k v) -> (k, v)) (EqSet.elems s)
+
+instance (Eq k, Eq v) => Eq (EqMap k v) where
+  EqMap set1 == EqMap set2 = set1 == set2
+
+instance (Show k, Show v) => Show (EqMap k v) where
+  show (EqMap s) = "{" ++ intercalate ", " (map (\(Arg k v) -> show k ++ "-> " ++ show v) (EqSet.elems s)) ++ "}"
+
+instance (Eq k) => Semigroup (EqMap k v) where
+  EqMap s1 <> EqMap s2 = EqMap (s1 <> s2)
+
+instance (Eq k) => Monoid (EqMap k v) where
+  mempty = empty
+  mappend = (<>)
+
 newtype CombiningMap k v = CombiningMap {getCombiningMap :: EqMap k v}
-instance (Eq k, Semigroup v) => Semigroup (CombiningMap k v)
-instance (Eq k, Semigroup v) => Monoid (CombiningMap k v)
+
+instance (Eq k, Semigroup v) => Semigroup (CombiningMap k v) where
+  CombiningMap map1 <> CombiningMap map2 = CombiningMap $ foldr combine map1 (assocs map2)
+    where
+      combine (k, v) acc =
+        EqMap.insert
+          k
+          ( case EqMap.lookup k acc of
+              Just vExisting -> vExisting <> v
+              Nothing -> v
+          )
+          acc
+
+instance (Eq k, Semigroup v) => Monoid (CombiningMap k v) where
+  mempty = CombiningMap empty
+  mappend :: (Eq k, Semigroup v) =>
+CombiningMap k v -> CombiningMap k v -> CombiningMap k v
+  mappend = (<>)
--- HW4/EqSet.hs
+++ HW4/EqSet.hs
@@ -2,14 +2,15 @@
 {-# OPTIONS_GHC -Wall -Werror #-}
 {-# OPTIONS_GHC -Wno-unused-imports #-}
 
-module EqSet (
-  EqSet,
-  empty,
-  EqSet.insert, -- To avoid name clash with Data.List.insert
-  member,
-  remove,
-  elems,
-) where
+module EqSet
+  ( EqSet,
+    empty,
+    EqSet.insert, -- To avoid name clash with Data.List.insert
+    member,
+    remove,
+    elems,
+  )
+where
 
 import Data.Either
 import Data.List
@@ -18,12 +19,31 @@ import Data.Maybe
 newtype EqSet a = EqSet {getSet :: [a]}
 
 empty :: EqSet a
-member :: Eq a => a -> EqSet a -> Bool
-insert :: Eq a => a -> EqSet a -> EqSet a
-remove :: Eq a => a -> EqSet a -> EqSet a
-elems :: EqSet a -> [a]
+empty = EqSet []
 
-instance Eq a => Eq (EqSet a)
-instance Show a => Show (EqSet a)
-instance Eq a => Semigroup (EqSet a)
-instance Eq a => Monoid (EqSet a)
+member :: (Eq a) => a -> EqSet a -> Bool
+member x (EqSet xs) = elem x xs
+
+insert :: (Eq a) => a -> EqSet a -> EqSet a
+insert x s@(EqSet xs) = if x `member` s then s else EqSet (x : xs)
+
+remove :: (Eq a) => a -> EqSet a -> EqSet a
+remove x (EqSet xs) = EqSet (delete x xs)
+
+elems :: EqSet a -> [a]
+elems (EqSet xs) = xs
+
+instance (Eq a) => Eq (EqSet a) where
+  EqSet xs == EqSet ys = all (`elem` ys) xs && all (`elem` xs) ys
+
+instance (Show a) => Show (EqSet a) where
+  show eqSet = "{" ++ intercalate ", " (map show (getSet eqSet)) ++ "}"
+
+instance (Eq a) => Semigroup (EqSet a) where
+  (EqSet xs) <> (EqSet ys) = EqSet $ foldr insertIfNotPresent ys xs
+    where
+      insertIfNotPresent x acc = if x `elem` acc then acc else x : acc
+
+instance (Eq a) => Monoid (EqSet a) where
+  mempty = empty
+  mappend = (<>)
